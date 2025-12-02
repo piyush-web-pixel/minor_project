df5dc6e80ba1379ad085fe729f3b88d087f583f4
piyush-web-pixel <piyushyadav6226@gmail.com>
2025-12-02 22:46:03 +0530

    changes

--- skill/settings.py
+++ skill/settings.py
@@ -82,24 +82,26 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Your top-level static folder
-]
-
-# Optional: enable compression for better performance
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-
-
-# # ----------------------
-# # STATIC & MEDIA FILES
-# # ----------------------
 # STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",  # Your top-level static folder
+# ]
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
+# # Optional: enable compression for better performance
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# ----------------------
+# STATIC & MEDIA FILES
+# ----------------------
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'jit' / 'static',
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
